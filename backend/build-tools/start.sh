75207f2d48b5b17cb5acf383c016d8cbf6ba2af1
jrayoub <aitounaayoub05@gmail.com>
2024-08-31 07:38:37 +0100

    changes

--- backend/build-tools/start.sh
+++ backend/build-tools/start.sh
@@ -1,3 +1,13 @@
+echo "Waiting for postgres..."
+until python3 manage.py dbshell < /dev/null; do
+  echo "Waiting for database to be ready..."
+  sleep 2
+done
+
+echo "PostgreSQL started"
+echo "makemigrations"
 python3 manage.py makemigrations
+echo "migrating ..."
 python3 manage.py migrate
+echo "Running $@"
 exec "$@"
---
+++ backend/payments/tests/test_setup.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from properties.models import Properties
+from users.models import Tenant
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.utils import timezone
+
+
+class UsersBaseTestCase(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.firstUser = get_user_model().objects.create_user(
+            username='firstUser', password='password', first_name='User', last_name='One')
+        self.secondUser = get_user_model().objects.create_user(
+            username='secondUser', password='password', first_name='User', last_name='Two')
+
+        self.property = Properties.objects.create(
+            name='Property 1',
+            address='123 Main St',
+            apartment_type='house',
+            number_of_units=10,
+            rental_cost=1000,
+            manager=self.firstUser)
+
+        self.tenant = Tenant.objects.create(
+            user=self.secondUser,
+            property=self.property,
+            section_occupied='A',
+            contract_start_date=timezone.now(),
+            contract_end_date=timezone.now() + timezone.timedelta(days=365)
+        )
+
+        self.AuthenticatedClient(self.firstUser)
+
+    def AuthenticatedClient(self, user):
+        refresh = RefreshToken.for_user(user)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f'Bearer {str(refresh.access_token)}')
